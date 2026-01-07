b53c776500701fdb825409eb80236096d1bb52e7
akira2705 <shivaathmajan@gmail.com>
2026-01-07 07:39:40 +0530

    Fix queue route and controller exports

--- BACKEND/src/routes/queue.routes.ts
+++ BACKEND/src/routes/queue.routes.ts
@@ -6,14 +6,6 @@ import {
   leaveQueue,
   seatFromQueue
 } from "../controllers/queue.controller";
-
-const router = Router();
-
-router.get("/", getQueue);
-router.get("/length", getQueueLength);
-router.post("/join", joinQueue);
-router.post("/leave", leaveQueue);
-router.put("/seat", seatFromQueue);
 import firebaseAuth from "../middlewares/firebaseAuth.middleware";
 import { requireRole } from "../middlewares/role.middleware";
 
