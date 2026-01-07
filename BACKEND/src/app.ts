cb1ab7acc6bd3c5fc73b823b4fc541c2d88b4992
akira2705 <shivaathmajan@gmail.com>
2026-01-07 10:39:46 +0530

    Add default db service export

--- BACKEND/src/app.ts
+++ BACKEND/src/app.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 import express, { Application } from "express";
 import cors from "cors";
-import { initializeDatabase } from "./services/db.service";
+import dbService from "./services/db.service";
 import userRoutes from "./routes/user.routes";
 import tableRoutes from "./routes/table.routes";
 import queueRoutes from "./routes/queue.routes";
@@ -14,7 +14,7 @@ import managerRoutes from "./routes/manager.routes";
 
 const app: Application = express();
 
-initializeDatabase().catch((error) => {
+dbService.initializeDatabase().catch((error: unknown) => {
   console.error("Database initialization failed", error);
 });
 
